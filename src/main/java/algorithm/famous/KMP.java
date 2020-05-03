c7857dfaaad4986e9a623809d9821adeb10cae1d
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-05-03 14:12:40 -0700

    Comment on KMP algorithm and refactor

--- src/main/java/algorithm/famous/KMP.java
+++ src/main/java/algorithm/famous/KMP.java
@@ -15,7 +15,24 @@ public class KMP {
      * This time improvement becomes possible with cost of preprocessing the pattern, inducing O(m) space complexity
      * while in the naive method, space complexity was O(1).
      *
-     * 
+     * The state machine for the pattern tells the algorithm that in case of a mismatch between pattern and text,
+     * how many characters from the beginning of the pattern can be skipped, so the algorithm doesn't need to backtrack
+     * on the text.
+     * This is the main reason that this algorithm works in such time.
+     *
+     * How the algorithm works?
+     * initialize state of the state machine to 0. We need this state to move between different states.
+     * Iterate over text, one character at a time, feed it to state machine and get the next state
+     * Check if the given text character matches with the expected character that state is pointing to
+     * If it does, then increase state by one (go to the next state in sequence) and go to the next character in text
+     * If it doesn't, then find another state and compare text character with that one.
+     * How to find another state?
+     * The state machine contains length of the longest prefix that is also a suffix of each substring
+     * (starting from the beginning of the pattern).
+     * If there is a mismatch, then algorithm looks at the previous state in sequence to find how many characters
+     * from the pattern it can skip, and it compare the text character with the next character after the skipped ones.
+     *
+     * It continues until the state gets to the final state, or the text ends.
      *
      * @param text input text to search through
      * @param p pattern to find within the text
@@ -23,21 +40,27 @@ public class KMP {
      */
     public static int find(String text, String p) {
         int[] states = createStateMachine(p);
-        int nextIndexToMatch = 0;
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c == p.charAt(nextIndexToMatch)) {
-                nextIndexToMatch++;
-            } else {
-                while (nextIndexToMatch > 0 && c != p.charAt(nextIndexToMatch)) {
-                    nextIndexToMatch = states[nextIndexToMatch - 1];
-                }
-            }
-            if (nextIndexToMatch == p.length()) {
-                return i - p.length() + 1;
-            }
+        int state = 0;
+        int textIndex = 0;
+        while (textIndex < text.length() && state < p.length()) {
+            state = nextState(states, text.charAt(textIndex), p, state);
+            textIndex++;
+        }
+        if (state == p.length()) {
+            return textIndex - p.length();
         }
         return -1;
+    }
+
+    private static int nextState(int[] stateMachine, char textCharacter, String pattern, int state) {
+        if (textCharacter == pattern.charAt(state)) {
+            state++;
+        } else {
+            while (state > 0 && textCharacter != pattern.charAt(state)) {
+                state = stateMachine[state - 1];
+            }
+        }
+        return state;
     }
 
 //    public static int[] createStateMachine(String p) {
@@ -53,6 +76,18 @@ public class KMP {
 //        return states;
 //    }
 
+    /**
+     * Each state is the length of the longest prefix that is also a suffix of the substring ending at the index of state
+     * Except for the whole sub-pattern. For example for "aaba",
+     * index 0: 0 there is no prefix for substring "a" that is also suffix of it and is shorter than "a" as well.
+     * index 1: 1 the length of the longest prefix of "aa" that is also suffix of it, and is shorter than the whole string
+     * index 2: 0, there is no prefix of "aab" that is also suffix of it
+     * index 3: 1, the length of the longest prefix of "aaba" that is also suffix of it and is shorter than "aaba" as well
+     * so the state machine would look like: "aaba"
+     *
+     * @param p given pattern
+     * @return state machine for the pattern
+     */
     public static int[] createStateMachine(String p) {
         int[] states = new int[p.length()];
         for (int i = 1; i < p.length(); i++) {
