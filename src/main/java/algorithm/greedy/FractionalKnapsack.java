3a8d0582b865425a8a3e56a77d06a86be05fb9a2
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-05-05 08:32:12 -0700

    Add comment for fractional knapsack problem

--- src/main/java/algorithm/greedy/FractionalKnapsack.java
+++ src/main/java/algorithm/greedy/FractionalKnapsack.java
@@ -10,6 +10,21 @@ public class FractionalKnapsack {
 
     private final static double epsilon = 0.001;
 
+    /**
+     * This problem has a greedy solution, meaning choosing a locally optimum solution for its sub-problems lead to
+     * globally optimum solution for the problem
+     *
+     * Move over the materials in decreasing order of their values per weight, take as much as you can
+     * Why it works? Very simple, let's say it doesn't lead to a globally optimum solution, then we can prove that
+     * by replacing one of the selected materials with a higher value, we get a better solution.
+     *
+     * Time complexity: O(nlogn), n number of materials
+     *
+     * @param weight how much we can take from the sack
+     * @param sack a mpa from value of each material (per unit of weight) to how much material we have for it
+     * @return maximum value we can get from the sack
+     * @throws IllegalArgumentException
+     */
     public static double choose(double weight, Map<Double, Double> sack) throws IllegalArgumentException {
         Preconditions.checkArgument(sack != null);
         Preconditions.checkArgument(weight > 0);
