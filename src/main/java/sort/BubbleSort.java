00801a18211b61cfcd38c389010e30d75dadd016
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-06-29 17:49:10 -0700

    Fix bubble sort. Add a comment

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -2,15 +2,28 @@ package sort;
 
 public class BubbleSort {
 
+
+    /**
+     * The elements bubbles up from bottom (left) to the surface (right) one element at a time.
+     * This is a stable sort. If we have two elements equal, the order is preserved.
+     * Space O(1), Time O(n ^ 2), for sorted or semi-sorted array: O(n)
+     * @param arr
+     * @param <T>
+     */
     public static <T extends Comparable<T>> void sort(T[] arr) {
         if (arr == null || arr.length == 0) {
             return;
         }
-        for (int i = 0; i < arr.length; i++) {
-            int index = i;
-            while (index > 0 && arr[index].compareTo(arr[index - 1]) < 0) {
-                swap(arr, index, index -1);
-                index--;
+        boolean keepOn = true;
+        for (int i = 0; i < arr.length && keepOn; i++) {
+            int index = 0;
+            keepOn = false;
+            while (index < arr.length - i - 1) {
+                if (arr[index].compareTo(arr[index + 1]) > 0) {
+                    swap(arr, index, index + 1);
+                    keepOn = true;
+                }
+                index++;
             }
         }
     }
