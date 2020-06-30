14ac77980c2a55a4eee0aec4a3cef172666e2286
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-06-30 12:58:13 -0700

    Simplify stack implementation

--- src/main/java/dataStructure/MyStack.java
+++ src/main/java/dataStructure/MyStack.java
@@ -7,38 +7,30 @@ import java.util.ArrayList;
 public class MyStack<T> {
 
     private ArrayList<T> arr;
-    private int next;
 
     public MyStack() {
-        this.next = 0;
         this.arr = new ArrayList<>();
     }
 
     public void push(T t) {
-        if (next == arr.size()) {
-            arr.add(null);
-        }
-        this.arr.set(next, t);
-        next++;
+        this.arr.add(0, t);
     }
 
     public T pop() {
-        Preconditions.checkArgument(next > 0, "Cannot pop an empty stack!");
-        T result = this.arr.get(next - 1);
-        next--;
-        return result;
+        Preconditions.checkArgument(!arr.isEmpty(), "Cannot pop an empty stack!");
+        return this.arr.remove(0);
     }
 
     public T top() {
-        Preconditions.checkArgument(next > 0, "No element in an empty stack");
-        return this.arr.get(next - 1);
+        Preconditions.checkArgument(!arr.isEmpty(), "No element in an empty stack");
+        return this.arr.get(0);
     }
 
     public long size() {
-        return next;
+        return this.arr.size();
     }
 
     public void clear() {
-        this.next = 0;
+        this.arr.clear();
     }
 }
