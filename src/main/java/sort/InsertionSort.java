69aca84d9f7383849edfed507130909d57877ef3
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-06-29 17:53:48 -0700

    Insertion sort

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,4 +1,31 @@
 package sort;
 
 public class InsertionSort {
+
+    /**
+     * It bubbles down. Each element on the left (starting from the closest element to the element at hand)
+     * is compared with the element at hand, and if it is larger it goes place right.
+     * Finally the element at hand is placed in the right position.
+     *
+     * Time complexity: O(N ^ 2), best case: if the array is semi-sorted
+     * Space complexity: O(N)
+     *
+     * @param arr
+     * @param <T>
+     */
+    public static <T extends Comparable<T>> void sort(T[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            T current = arr[i];
+            while (j > 0 && current.compareTo(arr[j - 1]) < 0) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = current;
+        }
+    }
+
 }
--- src/test/java/sort/InsertionSortUT.java
+++ src/test/java/sort/InsertionSortUT.java
@@ -1,4 +1,15 @@
 package sort;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InsertionSortUT {
+
+    @Test
+    public void testSort() {
+        Integer[] arr = new Integer[] {-5, 1, -10 , 7, 0, 8};
+        InsertionSort.sort(arr);
+        Assert.assertArrayEquals(new Integer[] {-10, -5, 0, 1, 7, 8}, arr);
+    }
+
 }
