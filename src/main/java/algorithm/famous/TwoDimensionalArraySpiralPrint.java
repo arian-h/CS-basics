e38f5f6fcdd0103b238d20703f713eefb0ec2e22
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-07-02 12:27:22 -0700

    Improve matrix spiral print

--- src/main/java/algorithm/famous/TwoDimensionalArraySpiralPrint.java
+++ src/main/java/algorithm/famous/TwoDimensionalArraySpiralPrint.java
@@ -9,54 +9,64 @@ public class TwoDimensionalArraySpiralPrint {
      * Time complexity: O(n), n being the number of cells in the array
      * Space complexity: O(n), n being the number of cells in the array, which is used for response object.
      * This algorithm is recursive. It simply gets a rectangle, and prints the outer layer of it.
-     * One should pay attention about the indices (not to go out of the given array, and not print twice)
-     * as well as row and column special cases
+     *
+     * Pay attention to the indices (not to go out of the given matrix, and not to print twice)
+     * as well as row and column special cases.
+     *
+     * It's easier to write this algorithm recursively and then convert it to iterative
+     *
      * @param matrix given 2 dimensional array
      * @param <T>
      * @return List of elements printed in spiral order
      */
     public static <T> List<T> print(T[][] matrix) {
-        List<T> result = new ArrayList<>();
-        print_sub(matrix, result, 0, matrix[0].length - 1, 0, matrix.length - 1);
-        return result;
+        List<T> traverse = new ArrayList<>();
+        int rLow = 0;
+        int rHigh = matrix.length - 1;
+        int cLow = 0;
+        int cHigh = matrix[0].length - 1;
+
+        while (rLow <= rHigh && cLow <= cHigh) {
+
+            // traverse the top row
+            int c = cLow;
+            while (c <= cHigh) {
+                traverse.add(matrix[rLow][c]);
+                c++;
+            }
+
+            // traverse the right column
+            int r = rLow + 1;
+            while (r <= rHigh) {
+                traverse.add(matrix[r][cHigh]);
+                r++;
+            }
+
+            if (rLow != rHigh) {
+                // traverse the bottom row
+                c = cHigh - 1;
+                while (c >= cLow) {
+                    traverse.add(matrix[rHigh][c]);
+                    c--;
+                }
+            }
+
+            if (cLow != cHigh) {
+                // traverse the left column
+                r = rHigh - 1;
+                while (r > rLow) {
+                    traverse.add(matrix[r][cLow]);
+                    r--;
+                }
+            }
+
+            rLow++;
+            cLow++;
+            cHigh--;
+            rHigh--;
+        }
+
+        return traverse;
     }
 
-    private static <T> void print_sub(T[][] matrix, List<T> list, int i, int j, int l, int h) {
-        if (i > j || l > h) {
-            return;
-        }
-        int x = i;
-        int y = l;
-        while (x <= j) {
-            list.add(matrix[y][x]);
-            x++;
-        }
-        x--;
-        y++;
-        // x = j
-        while (y <= h) {
-            list.add(matrix[y][x]);
-            y++;
-        }
-        y--;
-        x--;
-        if (l < h) {
-            // y = h
-            while (x >= i) {
-                list.add(matrix[y][x]);
-                x--;
-            }
-            x++;
-            y--;
-        }
-        // x = i
-        if (i < j) {
-            while (y > l) {
-                list.add(matrix[y][x]);
-                y--;
-            }
-            // y = l
-        }
-        print_sub(matrix, list, i + 1, j - 1, l + 1, h - 1);
-    }
 }
