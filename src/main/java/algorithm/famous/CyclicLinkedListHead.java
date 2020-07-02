cb570fe6664bb0b7a420e7405c64fd58e355b022
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-07-02 10:44:59 -0700

    Improve finding cyclic linkedlist cycle head

--- src/main/java/algorithm/famous/CyclicLinkedListHead.java
+++ src/main/java/algorithm/famous/CyclicLinkedListHead.java
@@ -7,18 +7,18 @@ public class CyclicLinkedListHead {
      * slow runner. If there is a cycle in the list, they will eventually meet.
      *
      * Let's say the length of the list from head to head of cycle is x, and the meeting point is y nodes from
-     * the cycle head and z nodes on the other side (it's a cycle!)
+     * the cycle head and z nodes on the other side (it's a cycle!), so the cycle is y + z nodes
      *
-     * for the slow runner, it took x + y nodes to the meeting point, for the fast runner it took x + y + z + y
-     * nodes to get to the meeting point
+     * for the slow runner, it takes x + y nodes to get to the meeting point, for the fast runner it takes x + y + z + y
+     * nodes:
      * 2 * (x + y) = x + y + z + y (fast runner ran twice the speed of slow runner)
      * ---> x = z
      *
-     * So the distance of the cycle head from head of the list is equal to the its distance to the meeting point
-     * (not y, but z)
+     * So the number of nodes from the list head to the cycle head is equal to the number of nodes from the
+     * meeting node to the cycle head.
      *
-     * therefore by moving two pointers (same speed, 1 node at a time), one from head, one from the meeting point
-     * we can find the cycle's head node
+     * Therefore by moving two pointers (same speed, 1 node at a time), one from head, another from the meeting point
+     * we can find the cycle's head node.
      *
      * Time complexity: O(n)
      * Space complexity: O(1)
@@ -27,33 +27,22 @@ public class CyclicLinkedListHead {
      * @return head of the cycle
      */
     public static <T> Node<T> findCycleHead(Node<T> head) {
-        Node<T> meetingNode = findMeetingNode(head);
-        if (meetingNode == null) {
-            return null;
-        }
-        Node<T> t = meetingNode;
-        Node<T> r = head;
-        while (t != r) {
-            t = t.next;
-            r = r.next;
-        }
-        return t;
-    }
-
-    private static <T> Node<T> findMeetingNode(Node<T> head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-        Node<T> slow = head.next;
-        Node<T> fast = head.next.next;
-        while (slow != null && fast != null && fast.next != null && fast != slow) {
+        Node<T> slow = head;
+        Node<T> fast = head;
+        do {
+            if (slow == null || fast == null || fast.next == null) {
+                return null;
+            }
             slow = slow.next;
             fast = fast.next.next;
+        } while (slow != fast);
+        // fast == slow
+        Node<T> current = head;
+        while (current != slow) {
+            slow = slow.next;
+            current = current.next;
         }
-        if (slow == null || fast == null || fast.next == null) {
-            return null;
-        }
-        return slow;
+        return current;
     }
 
     static class Node<T> {
