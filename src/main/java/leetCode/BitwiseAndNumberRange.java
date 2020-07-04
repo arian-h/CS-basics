fa811acea659b2a659167309ed2702cf509c2b29
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-07-04 16:11:10 -0700

    Improve bitwise-and-range

--- src/main/java/leetCode/BitwiseAndNumberRange.java
+++ src/main/java/leetCode/BitwiseAndNumberRange.java
@@ -3,9 +3,11 @@ package leetCode;
 public class BitwiseAndNumberRange {
 
     /**
-     * Write the numbers in binary format, there is a common prefix between these strings (binary representations)
-     * that doesn't change after and'ing all the numbers. the rest of them will change, because at least they
-     * became 0 once.
+     * https://leetcode.com/problems/bitwise-and-of-numbers-range/
+     *
+     * Write the numbers in binary format, their common prefix doesn't change.
+     * the rest of them will change, because at least they become 0 once in the path of and'ing.
+     *
      * Find the common prefix between the numbers, keep it and make the rest zero.
      *
      * @param m range lower bound
@@ -13,16 +15,28 @@ public class BitwiseAndNumberRange {
      * @return bitwise of numbers between m and n (inclusive)
      */
     public static int bitwiseAnd(int m, int n) {
-        int shift = 0;
-        while (m != n) {
-            m = m >> 1;
-            n = n >> 1;
-            shift++;
+        if (countDigits(n) != countDigits(m)) {
+            return 0;
         }
-        while (shift > 0) {
-            m = m << 1;
-            shift--;
+        int d = countDigits(n) - 1;
+        int k = 0;
+        while (d >= 0 && getDigit(m, d) == getDigit(n, d)) {
+            k <<= 1;
+            k += getDigit(m, d);
+            d--;
         }
-        return m;
+        while (d >= 0) {
+            k <<= 1;
+            d--;
+        }
+        return k;
+    }
+
+    private static int countDigits(int n) {
+        return (int) (Math.ceil(Math.log(n) / Math.log(2)));
+    }
+
+    private static int getDigit(int m, int i) {
+        return (m & (1 << i)) > 0 ? 1 : 0;
     }
 }
