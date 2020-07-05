d1e511c1f7bda6c0d0f5735ac62b32dfca24eb65
ahosseinzadeh <ahosseinzadeh@purestorage.com>
2020-07-05 16:44:46 -0700

    Add string shift comment

--- src/main/java/leetCode/StringShift.java
+++ src/main/java/leetCode/StringShift.java
@@ -2,6 +2,19 @@ package leetCode;
 
 public class StringShift {
 
+    /**
+     * You are given a string s containing lowercase English letters, and a matrix shift,
+     * where shift[i] = [direction, amount]:
+     *
+     * direction can be 0 (for left shift) or 1 (for right shift).
+     * amount is the amount by which string s is to be shifted.
+     * A left shift by 1 means remove the first character of s and append it to the end.
+     * Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
+     *
+     * @param s
+     * @param shifts
+     * @return
+     */
     public static String shift(String s, int[][] shifts) {
         char[] arr = s.toCharArray();
         int leftShifts = getLeftShifts(shifts, s.length());
